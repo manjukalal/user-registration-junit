ff2b60edec2521a2b2c5329e7ff00a9038475978
manjukalal <manjukalal1496@gmail.com>
2022-07-02 14:06:01 +0530

    Modified:validating emails for different patterns

--- src/test/java/org/example/UserRegistrationTest.java
+++ src/test/java/org/example/UserRegistrationTest.java
@@ -34,10 +34,59 @@ public class UserRegistrationTest {
 
 
     @Test
-    public void givenLoginRule1Values_WhenNotProper_ShouldReturnFalse() {
+    public void givenLoginRule1Values_WhenNotProper_ShouldReturnTrue() {
         boolean result = userRegistration.loginValidationPassword("Mn2@kalal");
         Assert.assertTrue(result);
 
 
+    }
+
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue1() {
+        boolean result = userRegistration.emailValidation("abc-100@yahoo.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue2() {
+        boolean result = userRegistration.emailValidation("abc.100@yahoo.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue3() {
+        boolean result = userRegistration.emailValidation("abc111@yahoo.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue4() {
+        boolean result = userRegistration.emailValidation("abc-100@abc.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue5() {
+        boolean result = userRegistration.emailValidation("abc@1.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue6() {
+        boolean result = userRegistration.emailValidation("abc+100@gmail.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue7() {
+        boolean result = userRegistration.emailValidation("manjukalal888@gmail.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue8() {
+        boolean result = userRegistration.emailValidation("abc@1.com");
+        Assert.assertTrue(result);
     }
 }
